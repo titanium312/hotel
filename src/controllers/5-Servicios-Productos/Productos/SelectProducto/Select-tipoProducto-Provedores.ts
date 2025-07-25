02223f79bc66dd02137f4ac7c7908da3dd94315b
rb-tb <rb.roberto.barreto@gmail.com>
2025-07-25 15:13:35 -0500

    --- ---

--- src/controllers/5-Servicios-Productos/Productos/SelectProducto/Select-tipoProducto-Provedores.ts
+++ src/controllers/5-Servicios-Productos/Productos/SelectProducto/Select-tipoProducto-Provedores.ts
@@ -42,7 +42,7 @@ export class ProductoController {
 
     try {
       // Realizamos la consulta SELECT para obtener los proveedores
-      const [proveedores] = await connection.query('SELECT * FROM provedores');
+      const [proveedores] = await connection.query('SELECT * FROM provedor');
 
       // Enviar la respuesta con los proveedores
       res.status(200).json({
